f16070de45f7468b01c14bd4152cfe60f03b1365
Loc Mai <locmai0315@gmail.com>
2023-01-11 01:23:27 -0500

    add concurrently & cors

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import puppeteer from "puppeteer";
 import express from 'express';
+import cors from 'cors';
 import dotenv from 'dotenv';
 dotenv.config();
 
 const app = express();
+app.use(cors());
 
 // scraper
 const fetchSwipes = async () => {
